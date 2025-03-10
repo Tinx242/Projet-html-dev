fea1d3bd712c86f8597644e9a768d749a18b5955
MARTHIN <likouangaclotairemarthin@gmail.com>
2025-03-10 17:12:35 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     console.log("loaded ok !!!");
+
+    let elTitre;
+    /*  elTitre = document.querySelector("title")
+     elTitre.innerText = "Développement PHP"
+     mainTitle.innerText += " et développement PHP" */
+    const mainTitle = document.querySelectorAll("title, h1");
+    mainTitle.forEach(el => el.innerText = "Développeur PHP");
 });
